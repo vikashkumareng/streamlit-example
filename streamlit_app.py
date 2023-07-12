8004e3477f3a3f908cde417bd75087096273e292
VIKASH KUMAR <107315146+vikashkumareng@users.noreply.github.com>
2023-07-12 18:09:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,64 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+# from collections import namedtuple
+# import altair as alt
+# import math
+# import pandas as pd
+# import streamlit as st
+
+# """
+# # Welcome to Streamlit!
+
+# Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
+
+# If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
+# forums](https://discuss.streamlit.io).
+
+# In the meantime, below is an example of what you can do with just a few lines of code:
+# """
+
+
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+
+#     Point = namedtuple('Point', 'x y')
+#     data = []
+
+#     points_per_turn = total_points / num_turns
+
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
+
+
 import streamlit as st
-
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+import os
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+os.environ["OPENAI_API_KEY"] = "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"
+llm = ChatOpenAI(temperature=0.0)
+memory = ConversationBufferMemory()
+conversation = ConversationChain(llm=llm, memory=memory, verbose=True)
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+def main():
+    st.title("Chatbot")
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+    user_input = st.text_input("User Input", "")
 
-    points_per_turn = total_points / num_turns
+    if st.button("Send"):
+        st.text("User: " + user_input)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+        response = conversation.predict(input=user_input)
+        st.text("Chatbot: " + response)
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+if __name__ == "__main__":
+    main()
